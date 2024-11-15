dc9a6de8005eec68944c0a82b4d31b256ff84dc0
Matyáš Pech <matyas.pech@gmail.com>
2024-11-15 15:34:35 +0100

    Enemy damage priprava

---
+++ Assets/Scripts/Interfaces/Idamagable.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface Idamagable
+{
+    public void TakeDamage(float damage);
+}
---
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour, Idamagable
+{
+    [SerializeField] float playerHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(float damage)
+    {
+        playerHealth -= damage;
+        if (playerHealth < 0)
+        {
+            Debug.Log("Lol chcipnul jsi");
+        }
+    }
+}
