8ca41620b2602b483a9d533c56125bce14c8a743
Matyáš Pech <matyas.pech@gmail.com>
2024-12-21 05:57:58 +0100

    clean up

--- Assets/Scripts/ParticleManager.cs
+++ Assets/Scripts/ParticleManager.cs
@@ -12,17 +12,10 @@ public class ParticleManager : MonoBehaviour
         string[] colors = { "green", "red", "yellow", "violet", "blue" };
         string randomColor = colors[Random.Range(0, colors.Length)];
 
-        Debug.Log($"Selected color name: {randomColor}");
-
         Color selectedColor = vfx.GetVector4(randomColor);
-
-        Debug.Log($"Retrieved color: {selectedColor}");
 
         vfx.SetVector4("MainColor", selectedColor);
         vfx.SetVector4("EmissionColor", selectedColor);
-
-        Debug.Log($"Main color: {vfx.GetVector4("MainColor")}");
-        Debug.Log($"Emission color: {vfx.GetVector4("EmissionColor")}");
 
         vfx.Play();
     }
