8287fe93e8dbb95afb949fd647e2cf28cc79a5e1
Denis <denis.resitko@gmail.com>
2025-02-15 23:06:14 +0100

    update

--- Assets/Scripts/MainMenu/PartSelection.cs
+++ Assets/Scripts/MainMenu/PartSelection.cs
@@ -1,43 +1,23 @@
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class PartSelection : MonoBehaviour
 {
-    private SelectablePart[] parts;
-    public SelectedWeaponsSO selectedWeaponsSO;
+    [SerializeField] private SelectedWeaponsSO selectedWeaponsSO;
+    [SerializeField] private Transform gunLHolder, gunRHolder, gunLPreview, gunRPreview;
+    [SerializeField] private GameObject[] gunPrefabs;
+    [SerializeField] private GameObject playBtn, backBtn, launchBtn, hintTxt;
+    [SerializeField] private CameraController cameraController;
+    [SerializeField] private Material outlineMAT;
+    [SerializeField] private GunSO gun1, gun2;
 
-    public Transform gunLHolder;
-    public Transform gunRHolder;
-    public Transform gunLPreview;
-    public Transform gunRPreview;
-
-    public GameObject[] gunPrefabs; // 0 - Gun1 | 1 - Gun2
-
-    public GameObject playBtn;
-    public GameObject backBtn;
-    public GameObject launchBtn;
-
-    private GameObject currentLGun;
-    private GameObject currentRGun;
-    private GameObject previewGun;
-
-    public CameraController cameraController;
-
-    public Material outlineMAT;
-
-    public bool isPreviewing = false;
-    public bool isPreviewingLeft = false;
-
-    public GunSO gun1;
-    public GunSO gun2;
-
-    public GameObject hintTxt;
+    private GameObject currentLGun, currentRGun, previewGun;
+    private bool isPreviewing, isPreviewingLeft;
 
 
     private void Start()
     {
-        InstantiateGun(gunLHolder, selectedWeaponsSO.gunL.gunName);
-        InstantiateGun(gunRHolder, selectedWeaponsSO.gunR.gunName);
+        InstantiateGun(gunLHolder, selectedWeaponsSO.gunL.gunName, ref currentLGun);
+        InstantiateGun(gunRHolder, selectedWeaponsSO.gunR.gunName, ref currentRGun);
 
         playBtn.GetComponent<PlayBtn>().PlayPressed += EnableGunColliders;
         backBtn.GetComponent<BackBtn>().BackPressed += DisableGunColliders;
@@ -48,144 +28,101 @@ public class PartSelection : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Mouse1) && isPreviewing) ExitPreview();
     }
 
-    void InstantiateGun(Transform holder, string gunName)
+    private void InstantiateGun(Transform holder, string gunName, ref GameObject currentGun)
     {
-        var gun = Instantiate(gunPrefabs[gunName == "Gun1" ? 0 : 1], holder).transform;
-        gun.localPosition = Vector3.zero;
-        gun.localRotation = Quaternion.identity;
+        var gun = Instantiate(gunPrefabs[gunName == "Gun1" ? 0 : 1], holder);
+        gun.transform.localPosition = Vector3.zero;
+        gun.transform.localRotation = Quaternion.identity;
 
-        gun.AddComponent<BoxCollider>();
-        gun.GetComponent<BoxCollider>().enabled = false;
-        gun.AddComponent<SelectablePart>();
-        gun.GetComponent<SelectablePart>().outliner = outlineMAT;
-        gun.GetComponent<SelectablePart>().clicked += PreviewParts;
+        var collider = gun.AddComponent<BoxCollider>();
+        collider.enabled = false;
 
-        if (holder == gunLHolder) currentLGun = gun.gameObject;
-        else currentRGun = gun.gameObject;
+        var part = gun.AddComponent<SelectablePart>();
+        part.outliner = outlineMAT;
+        part.clicked += PreviewParts;
+
+        currentGun = gun;
     }
 
-    void InstantiatePreviewGun(Transform holder, string gunName)
+    private void InstantiatePreviewGun(Transform holder, string gunName)
     {
-        var gun = Instantiate(gunPrefabs[gunName == "Gun1" ? 0 : 1], holder).transform;
-        gun.localPosition = Vector3.zero;
-        gun.localRotation = Quaternion.identity;
+        var gun = Instantiate(gunPrefabs[gunName == "Gun1" ? 0 : 1], holder);
+        gun.transform.localPosition = Vector3.zero;
+        gun.transform.localRotation = Quaternion.identity;
 
-        gun.AddComponent<BoxCollider>();
-        gun.GetComponent<BoxCollider>().enabled = true;
-        gun.AddComponent<PreviewPart>();
-        gun.GetComponent<PreviewPart>().outliner = outlineMAT;
-        gun.GetComponent<PreviewPart>().clicked = SwitchPart;
+        var collider = gun.AddComponent<BoxCollider>();
+        collider.enabled = true;
 
-        previewGun = gun.gameObject;
+        var preview = gun.AddComponent<PreviewPart>();
+        preview.outliner = outlineMAT;
+        preview.clicked = SwitchPart;
+
+        previewGun = gun;
     }
 
     private void SwitchPart(PreviewPart part)
     {
         if (isPreviewingLeft)
         {
-            Destroy(currentLGun);
-            Destroy(previewGun);
-
-            if (selectedWeaponsSO.gunL == gun1)
-            {
-                selectedWeaponsSO.gunL = gun2;
-                InstantiateGun(gunLHolder, selectedWeaponsSO.gunL.gunName);
-                InstantiatePreviewGun(gunLPreview, "Gun1");
-            }
-            else
-            {
-                selectedWeaponsSO.gunL = gun1;
-                InstantiateGun(gunLHolder, selectedWeaponsSO.gunL.gunName);
-                InstantiatePreviewGun(gunLPreview, "Gun2");
-            }
+            ReplaceGun(ref currentLGun, gunLHolder, gunLPreview, ref selectedWeaponsSO.gunL);
         }
         else
         {
-            Destroy(currentRGun);
-            Destroy(previewGun);
-
-            if (selectedWeaponsSO.gunR == gun1)
-            {
-                selectedWeaponsSO.gunR = gun2;
-                InstantiateGun(gunRHolder, selectedWeaponsSO.gunR.gunName);
-                InstantiatePreviewGun(gunRPreview, "Gun1");
-            }
-            else
-            {
-                selectedWeaponsSO.gunR = gun1;
-                InstantiateGun(gunRHolder, selectedWeaponsSO.gunR.gunName);
-                InstantiatePreviewGun(gunRPreview, "Gun2");
-            }
+            ReplaceGun(ref currentRGun, gunRHolder, gunRPreview, ref selectedWeaponsSO.gunR);
         }
     }
 
+    private void ReplaceGun(ref GameObject currentGun, Transform holder, Transform previewHolder, ref GunSO selectedGun)
+    {
+        Destroy(currentGun);
+        Destroy(previewGun);
+
+        selectedGun = selectedGun == gun1 ? gun2 : gun1;
+        InstantiateGun(holder, selectedGun.gunName, ref currentGun);
+        InstantiatePreviewGun(previewHolder, selectedGun == gun1 ? "Gun2" : "Gun1");
+    }
 
     private void PreviewParts(SelectablePart part)
     {
         isPreviewing = true;
-
         hintTxt.SetActive(true);
-
-        if (previewGun != null)
-            Destroy(previewGun);
-
+        Destroy(previewGun);
         backBtn.SetActive(false);
         launchBtn.SetActive(false);
 
         if (part.gameObject == currentLGun)
         {
             isPreviewingLeft = true;
-
-            currentLGun.GetComponent<BoxCollider>().enabled = false;
-            currentRGun.GetComponent<BoxCollider>().enabled = true;
+            SetGunColliders(false, true);
             cameraController.LerpCameraPos(2, CameraController.CameraPoints.GunLInspect, 1.75f);
-
-            if (selectedWeaponsSO.gunL.gunName == "Gun1")
-                InstantiatePreviewGun(gunLPreview, "Gun2");
-            else
-                InstantiatePreviewGun(gunLPreview, "Gun1");
+            InstantiatePreviewGun(gunLPreview, selectedWeaponsSO.gunL.gunName == "Gun1" ? "Gun2" : "Gun1");
         }
         else
         {
             isPreviewingLeft = false;
-
-            currentRGun.GetComponent<BoxCollider>().enabled = false;
-            currentLGun.GetComponent<BoxCollider>().enabled = true;
+            SetGunColliders(true, false);
             cameraController.LerpCameraPos(2, CameraController.CameraPoints.GunRInspect, 1.75f);
-
-            if (selectedWeaponsSO.gunR.gunName == "Gun1")
-                InstantiatePreviewGun(gunRPreview, "Gun2");
-            else
-                InstantiatePreviewGun(gunRPreview, "Gun1");
+            InstantiatePreviewGun(gunRPreview, selectedWeaponsSO.gunR.gunName == "Gun1" ? "Gun2" : "Gun1");
         }
     }
 
     private void ExitPreview()
     {
         isPreviewing = false;
-
         hintTxt.SetActive(false);
-
         Destroy(previewGun);
-
         backBtn.SetActive(true);
         launchBtn.SetActive(true);
-
         EnableGunColliders();
-
         cameraController.LerpCameraPos(2, CameraController.CameraPoints.PartSelection, 5.5f);
     }
 
-    private void EnableGunColliders()
-    {
-        currentLGun.GetComponent<BoxCollider>().enabled = true;
-        currentRGun.GetComponent<BoxCollider>().enabled = true;
-    }
+    private void EnableGunColliders() => SetGunColliders(true, true);
+    private void DisableGunColliders() => SetGunColliders(false, false);
 
-    private void DisableGunColliders()
+    private void SetGunColliders(bool left, bool right)
     {
-        currentLGun.GetComponent<BoxCollider>().enabled = false;
-        currentRGun.GetComponent<BoxCollider>().enabled = false;
+        currentLGun.GetComponent<BoxCollider>().enabled = left;
+        currentRGun.GetComponent<BoxCollider>().enabled = right;
     }
-
 }
